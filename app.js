486bd4af925751f57f1744f5421c9390c2d35b69
okashatanoli12345@gmail.com <64710012+OkashaTanoli@users.noreply.github.com>
2023-04-29 13:24:30 +0500

    cors updated

--- app.js
+++ app.js
@@ -9,7 +9,10 @@ const articleshistory = require('./routes/articlesHistory.js')
 const app = express()
 const PORT = process.env.PORT || 5000
 
-app.use(cors())
+app.use(cors({
+    origin: '*'
+}));
+
 app.use(express.json())
 
 app.use("/api/v1/user", user)
