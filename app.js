51241086c89dec5563471a4ef8be15a9aed91d36
okashatanoli12345@gmail.com <64710012+OkashaTanoli@users.noreply.github.com>
2023-04-29 20:07:01 +0500

    cors updated

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const app = express()
 const PORT = process.env.PORT || 5000
 
 app.use(cors({
-    origin: '*'
+    origin: 'https://chatgpt-content-writer.vercel.app/'
 }));
 
 app.use(express.json())
